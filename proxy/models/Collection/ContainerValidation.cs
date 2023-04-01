6568e5c93ab85667c20b761f73784ef8d5a5493a
khrystyna-kolba <hrystyna621@gmail.com>
2023-04-01 18:42:36 +0300

    created proxy task

---
+++ proxy/models/Collection/ContainerValidation.cs
@@ -0,0 +1,83 @@
+﻿using pattern_proxy_np.Extension;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace pattern_proxy_np.models.Collection
+{
+    public class ContainerValidation
+    {
+        public static DateTime ArrivalDateValidation(DateTime s, DateTime l)
+        {
+            if (s > l)
+            {
+                throw new ArgumentException("departure date can't be later than arrival date");
+            }
+            return l;
+        }
+        public static string NumberValidation(string num)
+        {
+            Regex re = new Regex(@"^[A-Z]{2}-[0-9]{5}$");
+            if (!re.IsMatch(num))
+            {
+                throw new ArgumentException("number should follow pattern AB-12345");
+            }
+            return num;
+        }
+        public static string IdValidation(string id)
+        {
+            foreach (var c in id)
+            {
+                if (!char.IsDigit(c))
+                {
+                    throw new ArgumentException("id should contain only digits");
+                }
+            }
+            return id;
+        }
+        public static DateTime DateValidation(string dt)
+        {
+            DateTime res;
+            if (DateTime.TryParse(dt, out res))
+            {
+                return res;
+            }
+            throw new ArgumentException("date is invalid");
+        }
+
+        public static City CityValidation(string city)
+        {
+            object res;
+            try
+            {
+                City c = new City();
+                return c.ParseEnum(city);
+            }
+            catch
+            {
+                throw new ArgumentException("city is invalid");
+            }
+        }
+        public static int IntValidation(string amount)
+        {
+            if (!int.TryParse(amount, out int res))
+            {
+                throw new ArgumentException("field should be integer");
+            }
+            return res;
+        }
+        public static int AmountValidation(int amount)
+        {
+            if (amount >= 0)
+            {
+                return amount;
+            }
+            throw new ArgumentException("field should be non-negative!");
+        }
+    }
+}
---
+++ proxy/models/Proxy/PermissionProxy.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using pattern_proxy_np.models.Authentification;
+using pattern_proxy_np.models.Collection;
+
+namespace pattern_proxy_np.models.Proxy
+{
+    public class PermissionProxy : ISubject
+    {
+        private RealSubject subject;
+        private Auth auth;
+        public PermissionProxy(RealSubject subject, Auth auth)
+        {
+            this.subject = subject;
+            this.auth = auth;
+        }
+        public Collection<Container> Containers { get { return subject.Containers; } }
+        public User CurrentUser { get => auth.CurrentUser; }
+        public bool CheckAdminAccess()
+        {
+            if (auth.CurrentUser != null)
+            {
+                return auth.CurrentUser.Role == Role.admin;
+            }
+            throw new AccessViolationException("forbidden");
+        }
+        public bool CheckCustomerAccess()
+        {
+            if (auth.CurrentUser != null)
+            {
+                return auth.CurrentUser.Role == Role.customer;
+            }
+            throw new AccessViolationException("forbidden");
+        }
+        public Collection<Container>? Search(string s)
+        {
+            if (CheckCustomerAccess() || CheckAdminAccess())
+            {
+                return subject.Search(s);
+            }
+            return null;
+        }
+        public Collection<Container> Sort(string sort_by)
+        {
+            if (CheckCustomerAccess() || CheckAdminAccess())
+            {
+                return subject.Sort(sort_by);
+            }
+            return null;
+        }
+        public Container ViewById(string id)
+        {
+            if (CheckCustomerAccess() || CheckAdminAccess())
+            {
+                return subject.ViewById(id);
+            }
+            return null;
+        }
+        public Collection<Container> ViewList()
+        {
+            if (CheckCustomerAccess() || CheckAdminAccess())
+            {
+                return subject.ViewList();
+            }
+            return null;
+        }
+        public Container Edit(string id, string prop, string new_value)
+        {
+            if (CheckAdminAccess())
+            {
+                return subject.Edit(id, prop, new_value);
+            }
+            return null;
+        }
+        public Container Delete(string id)
+        {
+            if (CheckAdminAccess())
+            {
+                return subject.Delete(id);
+            }
+            return null;
+        }
+        public Container Create()
+        {
+            if (CheckAdminAccess())
+            {
+                return subject.Create();
+            }
+            return null;
+        }
+        public void ReadFromJsonFile(string fileName)
+        {
+            if (CheckAdminAccess())
+            {
+                subject.ReadFromJsonFile(fileName);
+            }
+        }
+        public void WriteToJsonFile(string fileName)
+        {
+            if (CheckAdminAccess())
+            {
+                subject.WriteToJsonFile(fileName);
+            }
+        }
+    }
+}
